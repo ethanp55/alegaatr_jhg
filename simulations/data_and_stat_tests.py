1864260f25188a163853e393136439ca9ad82d72
Ethan Pedersen <ethanp55@gmail.com>
2024-07-11 12:37:45 -0700

    Improve stat test code and update to store formatted data for plots

--- simulations/data_and_stat_tests.py
+++ simulations/data_and_stat_tests.py
@@ -4,6 +4,8 @@ import os
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 MAX_N_PLAYERS = 20
+SAVE_DATA = False
+FINAL_POPS, PERCENTILES, WINS, POP_SUMS = True, False, False, False
 
 
 def _calculate_percentile(array, value):
@@ -14,36 +16,52 @@ def _calculate_percentile(array, value):
 
 
 results, folder = {}, '../simulations/results/'
+results['overall'] = {}
+names, pop_conditions, num_players, num_rounds, num_cats = [], [], [], [], []
+pop_sums, final_pops, percentiles, wins = [], [], [], []
 
 for file in os.listdir(folder):
     agent_name = file.split('_')[0]
-    if agent_name == 'DQN':  # TODO: remove this
-        continue
     pop_condition = file.split('pop=')[1].split('_')[0]
     n_players = file.split('p=')[2].split('_')[0]
     n_rounds = file.split('r=')[1].split('_')[0]
     n_cats = file.split('c=')[1].split('_')[0][0]
+    data = np.genfromtxt(f'{folder}{file}', delimiter=',', skip_header=0)
 
-    # if f'pop={pop_condition}' not in results:
-    #     results[f'pop={pop_condition}'] = {}
+    if SAVE_DATA:
+        for row in data:
+            # Add the condition info
+            names.append(agent_name)
+            pop_conditions.append(pop_condition)
+            num_players.append(n_players)
+            num_rounds.append(n_rounds)
+            num_cats.append(n_cats)
+
+            # Calculate and add the results
+            agent_pop = row[-1]
+            pop_sums.append(sum(row))
+            final_pops.append(agent_pop)
+            percentiles.append(_calculate_percentile(row, agent_pop))
+            wins.append(1 if agent_pop == row.max() else 0)
+
+    if f'pop={pop_condition}' not in results:
+        results[f'pop={pop_condition}'] = {}
     if f'p={n_players}' not in results:
         results[f'p={n_players}'] = {}
     if f'r={n_rounds}' not in results:
         results[f'r={n_rounds}'] = {}
     if f'c={n_cats}' not in results:
         results[f'c={n_cats}'] = {}
-    if 'overall' not in results:
-        results['overall'] = {}
 
-    data = np.genfromtxt(f'{folder}{file}', delimiter=',', skip_header=0)
     n_rows, n_cols = data.shape
     if n_cols < MAX_N_PLAYERS:
         n_zeroes = MAX_N_PLAYERS - n_cols
         zeroes = np.zeros((n_rows, n_zeroes))
         data = np.concatenate((zeroes, data), axis=1)
 
-    # results[f'pop={pop_condition}'][agent_name] = np.concatenate(
-    #     (results[f'pop={pop_condition}'][agent_name], data)) if agent_name in results[f'pop={pop_condition}'] else data
+    results[f'pop={pop_condition}'][agent_name] = np.concatenate(
+        (results[f'pop={pop_condition}'][agent_name], data)) if agent_name in results[
+        f'pop={pop_condition}'] else data
     results[f'p={n_players}'][agent_name] = np.concatenate(
         (results[f'p={n_players}'][agent_name], data)) if agent_name in results[f'p={n_players}'] else data
     results[f'r={n_rounds}'][agent_name] = np.concatenate(
@@ -53,6 +71,24 @@ for file in os.listdir(folder):
     results['overall'][agent_name] = np.concatenate(
         (results['overall'][agent_name], data)) if agent_name in results['overall'] else data
 
+# Store in a csv file for analysis in Google Sheets (or MS Excel)
+if SAVE_DATA:
+    df = pd.DataFrame(
+        {
+            'algorithm': names,
+            'pop_condition': pop_conditions,
+            'n_players': num_players,
+            'n_rounds': num_rounds,
+            'n_cats': num_cats,
+            'society_pop_sum': pop_sums,
+            'agent_final_pop': final_pops,
+            'agent_percentile': percentiles,
+            'agent_won': wins
+        }
+    )
+    df.to_csv('../simulations/formatted_results.csv', index=False)
+
+# Run comparison tests
 for scenario_str, scenario_results in results.items():
     names, pop_sums, final_pops, percentiles, wins = [], [], [], [], []
 
@@ -69,21 +105,25 @@ for scenario_str, scenario_results in results.items():
     print(f'STATISTICAL TESTS FOR {scenario_str} SCENARIOS')
     print('-----------------------------------------------')
 
-    print('FINAL POPS:')
-    df = pd.DataFrame({'algorithm': names, 'results': final_pops})
-    print(pairwise_tukeyhsd(endog=df['results'], groups=df['algorithm'], alpha=0.05))
+    if FINAL_POPS:
+        print('\nFINAL POPS:')
+        df = pd.DataFrame({'algorithm': names, 'results': final_pops})
+        print(pairwise_tukeyhsd(endog=df['results'], groups=df['algorithm'], alpha=0.05))
 
-    print('\nPERCENTILES:')
-    df = pd.DataFrame({'algorithm': names, 'results': percentiles})
-    print(pairwise_tukeyhsd(endog=df['results'], groups=df['algorithm'], alpha=0.05))
+    if PERCENTILES:
+        print('\nPERCENTILES:')
+        df = pd.DataFrame({'algorithm': names, 'results': percentiles})
+        print(pairwise_tukeyhsd(endog=df['results'], groups=df['algorithm'], alpha=0.05))
 
-    print('\nWINS:')
-    df = pd.DataFrame({'algorithm': names, 'results': wins})
-    print(pairwise_tukeyhsd(endog=df['results'], groups=df['algorithm'], alpha=0.05))
+    if WINS:
+        print('\nWINS:')
+        df = pd.DataFrame({'algorithm': names, 'results': wins})
+        print(pairwise_tukeyhsd(endog=df['results'], groups=df['algorithm'], alpha=0.05))
 
-    print('\nPOP SUMS:')
-    df = pd.DataFrame({'algorithm': names, 'results': pop_sums})
-    print(pairwise_tukeyhsd(endog=df['results'], groups=df['algorithm'], alpha=0.05))
+    if POP_SUMS:
+        print('\nPOP SUMS:')
+        df = pd.DataFrame({'algorithm': names, 'results': pop_sums})
+        print(pairwise_tukeyhsd(endog=df['results'], groups=df['algorithm'], alpha=0.05))
 
     print('-----------------------------------------------')
     print('-----------------------------------------------')
